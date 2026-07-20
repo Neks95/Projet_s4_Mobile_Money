70b3e8b01d9338baeef751e27283a3348dfa0f9d
Neks95 <rakotomangaa.nekena@gmail.com>
2026-07-20 13:06:15 +0300

    retrait+historique

--- app/Models/OperationModel.php
+++ app/Models/OperationModel.php
@@ -4,14 +4,29 @@ use CodeIgniter\Model;
 
 class OperationModel extends Model {
     protected $table = 'operation';
-    
+    protected $primaryKey = 'id';
+    protected $allowedFields = [
+        'id_client1', 
+        'id_client2', 
+        'id_type_operation', 
+        'date_operation', 
+        'montant', 
+        'frais_applique'
+    ];
+
     public function getRecentes($idClient, $limit = 5) {
-        return $this->where('id_client1', $idClient)
+    return $this->select('operation.*, type_operation.libelle')
+                ->join('type_operation', 'type_operation.id = operation.id_type_operation')
+                ->groupStart()
+                    ->where('id_client1', $idClient)
                     ->orWhere('id_client2', $idClient)
-                    ->orderBy('date_operation', 'DESC')
-                    ->limit($limit)
-                    ->findAll();
-    }
+                ->groupEnd()
+                ->orderBy('date_operation', 'DESC')
+                ->limit($limit)
+                ->findAll();
+}
+    
+  
 }
 
 ?>
